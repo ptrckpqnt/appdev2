84919e4e4aeeed38db3c7c02a4c00713d90871f3
ptrckpqnt <patrickpioquinto2325.com>
2026-02-13 01:29:11 +0800

    Done with justify content section

--- react-native-flexbox/index.js
+++ react-native-flexbox/index.js
@@ -2,9 +2,10 @@ import { registerRootComponent } from 'expo';
 
 // import Flex from './components/Flex';
 // import FlexDirectionBasics from './components/FlexDirectionBasics';
-import DirectionLayout from './components/DirectionLayout';
+// import DirectionLayout from './components/DirectionLayout';
+import JustifyContentBasics from './components/JustifyContentBasics';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(DirectionLayout);
+registerRootComponent(JustifyContentBasics);
