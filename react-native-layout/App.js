3c003eb0714f938d2a6d65eccd3419da722c1ed1
ptrckpqnt <patrickpioquinto2325.com>
2026-02-18 07:14:14 +0800

    Done with Sign Up screen

--- react-native-layout/App.js
+++ react-native-layout/App.js
@@ -1,8 +1,9 @@
 import Login from "./src/screens/Login";
+import Signup from "./src/screens/Signup";
 
 export default function App() {
   return (
-    <Login/>
+    <Signup/>
   );
 }
 
---
+++ react-native-layout/src/screens/Signup.js
@@ -0,0 +1,116 @@
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function Signup() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Create Account</Text>
+
+      <View style={styles.inputContainer}>
+        <Ionicons
+          name="person-outline"
+          size={20}
+          color="#666"
+          style={styles.icon}
+        />
+        <TextInput placeholder="Username" style={styles.input} />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Ionicons
+          name="mail-outline"
+          size={20}
+          color="#666"
+          style={styles.icon}
+        />
+        <TextInput placeholder="Enter email address" style={styles.input} />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Ionicons
+          name="lock-closed-outline"
+          size={20}
+          color="#666"
+          style={styles.icon}
+        />
+        <TextInput
+          placeholder="Enter password"
+          secureTextEntry
+          style={styles.input}
+        />
+      </View>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.footerText}>
+        Already have an account?{" "}
+        <Text style={styles.loginText}>Login</Text>
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 30,
+    color: "#333",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 50,
+    paddingHorizontal: 20,
+    marginBottom: 15,
+    width: "100%",
+    height: 50,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+  },
+  button: {
+    backgroundColor: "#28a745", 
+    padding: 15,
+    borderRadius: 50,
+    width: "100%",
+    alignItems: "center",
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+   footerText: {
+    color: "#666",
+    fontSize: 14,
+    textAlign: "center",
+    marginTop: 20,
+  },
+
+  loginText: {
+    color: "#2c77e9",
+    fontWeight: "bold",
+  },
+});
